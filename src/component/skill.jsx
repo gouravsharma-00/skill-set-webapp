ffecbc8386d9956ece8c0e01bbeed7e508d80488
Gourav Sharma <gourav.sharma.glamat2932@gmail.com>
2024-08-10 00:02:56 +0530

    added dialogue

--- src/component/skill.jsx
+++ src/component/skill.jsx
@@ -1,23 +1,34 @@
 export default function Skill({ domain, bool }) {
-
+    const handleClick = () => {
+        document.getElementById('model').showModal();
+    }
+    const handleClose = () => {
+        document.getElementById('model').close();
+    }
     return(
         bool && <div id="button" className="flex">
             {
                 domain.skill.map((skill) => {
                     return(
-                        <button key={crypto.randomUUID()}>{skill}</button>
+                        <button key={crypto.randomUUID()} className="buttonskill">{skill}</button>
                     )
                 })
             }
-            <div>
-                <ol>
-                    <li>W3school</li>
-                    <li>Official documentation </li>
-                    <li>YouTube code with hary, web dev simplified, bro code and etc.</li>
-                    <li>Class notes by Devendra sir.</li>
-                    <li>Online resources and website like Odin Project and github, stakeOverflow</li>
-                </ol>
-            </div>
+           <div>
+           <p id="arrow" onClick={handleClick}>show resources&#8594;</p>
+           </div>
+           <dialog id='model'>
+            <ol>
+                <li><a href='https://www.w3schools.com/'>W3School</a></li>
+                <li><a href='https://www.udemy.com/'>Udemy</a></li>
+                <li><a href='https://www.coursera.org/'>coursera</a></li>
+                <li><a href='https://www.youtube.com/'>Youtube</a></li>
+                <li><a href='https://archive.org/details/texts'>Books</a></li>
+                <li><a href='#'>Devindra sir notes</a></li>
+            </ol>
+            <span>Note: Resources are not limited to these you may find other useful resources on the <a href='https://www.google.co.in/'>internet</a></span>
+            <button class="button" id='submit' data-modal-close onClick={handleClose}>Close</button>
+        </dialog>
         </div>
     )
 }
